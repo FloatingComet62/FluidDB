98ad33abc3544cf513260c7c0e97567a584f39ae
FloatingComet62 <shubh2007rai@gmail.com>
2022-04-08 09:57:56 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*
+    TODO userauth
+    TODO api routes
+    TODO database (low.js for low level interaction) (high.js for high level interaction)
+    ? dom?
+
+    * Database > Ocean(clusters) > Sea(collections) > River(documents)
+*/
