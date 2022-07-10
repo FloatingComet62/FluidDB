686f22969e5e01ed944fe0cb5ab0a5cb3230f93e
FloatingComet62 <shubh2007rai@gmail.com>
2022-07-10 20:55:00 +0530

    Update and Delete... I think the api is done....

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,10 @@ app.post('/ocean/:oceanName', (req, res) => {
     const { success, message, code } = high.createOcean(req.params.oceanName)
     endHandler(success, message, req, res, 201, code)
 })
+app.delete('/ocean/:oceanName', (req, res) => {
+    const { success, message, code } = high.deleteOcean(req.params.oceanName)
+    endHandler(success, message, req, res, 200, code)
+})
 
 app.get('/sea/:oceanName/:seaName', (req, res) => {
     const { success, message, code } = high.getSea(
@@ -52,6 +56,13 @@ app.post('/sea/:oceanName/:seaName', (req, res) => {
         req.params.seaName
     )
     endHandler(success, message, req, res, 201, code)
+})
+app.delete('/sea/:oceanName/:seaName', (req, res) => {
+    const { success, message, code } = high.deleteSea(
+        req.params.oceanName,
+        req.params.seaName
+    )
+    endHandler(success, message, req, res, 200, code)
 })
 
 app.get('/river/:oceanName/:seaName/:riverName', (req, res) => {
@@ -71,3 +82,22 @@ app.post('/river/:oceanName/:seaName/:riverName/:value', (req, res) => {
     )
     endHandler(success, message, req, res, 201, code)
 })
+app.delete('/river/:oceanName/:seaName/:riverName', (req, res) => {
+    const { success, message, code } = high.deleteRiver(
+        req.params.oceanName,
+        req.params.seaName,
+        req.params.riverName
+    )
+    endHandler(success, message, req, res, 200, code)
+})
+app.put('/river/:oceanName/:seaName/:riverName/:value', (req, res) => {
+    const { success, message, code } = high.updateRiver(
+        req.params.oceanName,
+        req.params.seaName,
+        req.params.riverName,
+        req.params.value
+    )
+    endHandler(success, message, req, res, 200, code)
+})
+
+app.use('*', (_, res) => res.status(404).json({ success: false, message: 'Not found', systemCode: 1 }))
